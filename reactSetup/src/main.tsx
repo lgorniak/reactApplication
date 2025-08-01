87f36f70508f7098e3d1a6727bb693c008c19f50
Lukas Gorniak <lukas.gorniak@generic.de>
2025-08-01 11:35:43 +0200

    Update React application with ECharts integration and UI improvements

--- reactSetup/src/main.tsx
+++ reactSetup/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+import "primereact/resources/themes/md-dark-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+
 // Register ECharts modules before any chart rendering
 import { registerEChartsModules } from "./lib/echarts";
 registerEChartsModules();
